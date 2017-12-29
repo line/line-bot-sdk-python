4371daef398d9fda1446d78e5a0556939d833561
Nobuhiro MIKI <nob@bobuhiro11.net>
2017-12-29 14:06:56 +0900

    Fix arg_parser option for examples (#87)

--- examples/simple-server-echo/app.py
+++ examples/simple-server-echo/app.py
@@ -96,7 +96,7 @@ if __name__ == '__main__':
     arg_parser = ArgumentParser(
         usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
     )
-    arg_parser.add_argument('-p', '--port', default=8000, help='port')
+    arg_parser.add_argument('-p', '--port', type=int, default=8000, help='port')
     options = arg_parser.parse_args()
 
     httpd = wsgiref.simple_server.make_server('', options.port, application)

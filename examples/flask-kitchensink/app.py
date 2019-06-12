c43971137135ee649fd9c7cbd917928e848680d2
okue <nogideca@gmail.com>
2019-06-12 18:56:01 +0900

    text property of Postback action is deprecated; and fix example

--- examples/flask-kitchensink/app.py
+++ examples/flask-kitchensink/app.py
@@ -109,7 +109,7 @@ def handle_text_message(event):
             line_bot_api.reply_message(
                 event.reply_token, [
                     TextSendMessage(text='Display name: ' + profile.display_name),
-                    TextSendMessage(text='Status message: ' + profile.status_message)
+                    TextSendMessage(text='Status message: ' + str(profile.status_message))
                 ]
             )
         else:

39c83441f6972a69dec076b129e286ea9bf35bcb
okue <nogideca@gmail.com>
2019-10-25 17:55:58 +0900

    fix docstring describing module name

--- linebot/webhook.py
+++ linebot/webhook.py
@@ -12,7 +12,7 @@
 #  License for the specific language governing permissions and limitations
 #  under the License.
 
-"""linebot.http_client webhook."""
+"""linebot.webhook module."""
 
 from __future__ import unicode_literals
 

db8a8804c381af11e8450f533f2380b87bb7ee2b
NiJia <louis70109@gmail.com>
2020-04-16 00:31:16 +0800

    fix: typo

--- linebot/models/sticon.py
+++ linebot/models/sticon.py
@@ -12,7 +12,7 @@
 #  License for the specific language governing permissions and limitations
 #  under the License.
 
-"""linebot.models.recipient module."""
+"""linebot.models.sticon module."""
 
 from __future__ import unicode_literals
 

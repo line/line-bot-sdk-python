4f900fe442ad7cc3fa8db0d4a83b8c9b40948955
NiJia Lin <louis70109@gmail.com>
2020-12-14 18:05:22 +0800

    Release 1.18.0

--- linebot/__about__.py
+++ linebot/__about__.py
@@ -16,7 +16,7 @@
 
 from __future__ import unicode_literals
 
-__version__ = '1.17.0'
+__version__ = '1.18.0'
 __author__ = 'LINE Corporation'
 __copyright__ = 'Copyright 2016, LINE Corporation'
 __license__ = 'Apache 2.0'

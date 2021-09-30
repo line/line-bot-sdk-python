75843dec4d605ee56e207df1e59993a045563018
Tokuhiro Matsuno <tokuhirom@gmail.com>
2021-09-30 13:27:56 +0900

    Official required version is Python 3.6+

--- setup.py
+++ setup.py
@@ -69,6 +69,7 @@ setup(
     packages=[
         "linebot", "linebot.models"
     ],
+    python_requires=">=3.6.0",
     install_requires=_requirements(),
     tests_require=_requirements_test(),
     cmdclass={'test': PyTest},
@@ -77,14 +78,10 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Intended Audience :: Developers",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Software Development"
     ]
 )

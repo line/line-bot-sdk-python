cf41ba42523da2966b5334f94ce6251fce55a0ef
nanato12 <admin@nanato12.info>
2022-05-16 12:27:39 +0900

    update: PostbackInputOption

--- linebot/constants/postback_input_option.py
+++ linebot/constants/postback_input_option.py
@@ -14,6 +14,7 @@
 
 
 class PostbackInputOption:
+    """Constant class for Postback input option"""
     CLOSE_RICH_MENU = "closeRichMenu"
     OPEN_RICH_MENU = "openRichMenu"
     OPEN_KEYBOARD = "openKeyboard"

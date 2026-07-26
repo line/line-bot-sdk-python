72755eda95734cba27d7327568859dfddd89edbd
tonafkilo9-ship-it <tonafkilo9@gmail.com>
2026-07-26 23:38:39 +0700

    Update app.py

--- examples/flask-echo/app.py
+++ examples/flask-echo/app.py
@@ -1,98 +1,45 @@
-# -*- coding: utf-8 -*-
-
-#  Licensed under the Apache License, Version 2.0 (the "License"); you may
-#  not use this file except in compliance with the License. You may obtain
-#  a copy of the License at
-#
-#       https://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#  License for the specific language governing permissions and limitations
-#  under the License.
-
-
 import os
-import sys
-from argparse import ArgumentParser
-
+import re
 from flask import Flask, request, abort
-from linebot import (
-    WebhookParser
-)
-from linebot.v3.exceptions import (
-    InvalidSignatureError
-)
-from linebot.v3.webhooks import (
-    MessageEvent,
-    TextMessageContent,
-)
-from linebot.v3.messaging import (
-    Configuration,
-    ApiClient,
-    MessagingApi,
-    ReplyMessageRequest,
-    TextMessage
-)
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage
 
 app = Flask(__name__)
 
-# get channel_secret and channel_access_token from your environment variable
-channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
-channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
-if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
-if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
+# ดึง Token และ Secret จาก Environment Variable ของ Render อัตโนมัติ
+line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
 
-parser = WebhookParser(channel_secret)
-
-configuration = Configuration(
-    access_token=channel_access_token
-)
-
+# ตัวแปรควบคุมโหมดสแตนด์บาย (เริ่มต้นปิดไว้)
+IS_STANDBY = False 
 
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
-
-    # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # parse webhook body
     try:
-        events = parser.parse(body, signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
-    # if event is MessageEvent and message is TextMessage, then echo text
-    for event in events:
-        if not isinstance(event, MessageEvent):
-            continue
-        if not isinstance(event.message, TextMessageContent):
-            continue
-        with ApiClient(configuration) as api_client:
-            line_bot_api = MessagingApi(api_client)
-            line_bot_api.reply_message_with_http_info(
-                ReplyMessageRequest(
-                    reply_token=event.reply_token,
-                    messages=[TextMessage(text=event.message.text)]
-                )
-            )
-
     return 'OK'
 
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    global IS_STANDBY
+    
+    # ถ้ายังไม่เปิดสแตนด์บาย จะไม่สนใจข้อความใดๆ ทั้งสิ้น
+    if not IS_STANDBY:
+        return
+
+    user_message = event.message.text.strip()
+    user_id = event.source.user_id
+    
+    # กรองเฉพาะข้อความที่เป็นรหัสงาน (ขึ้นต้นด้วย WC, WG, WK ตามด้วยตัวเลข)
+    if re.match(r'^(WC|WG|WK)\d+', user_message, re.IGNORECASE):
+        print(f"เจอและเก็บรหัสงาน: {user_message} จากผู้ใช้: {user_id}")
+    else:
+        return
 
 if __name__ == "__main__":
-    arg_parser = ArgumentParser(
-        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    )
-    arg_parser.add_argument('-p', '--port', type=int, default=8000, help='port')
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    options = arg_parser.parse_args()
-
-    app.run(debug=options.debug, port=options.port)
+    app.run()
